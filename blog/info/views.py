d610a59661c27ecaf4c69e503472fec93490d40f
xrig <xrig@pop-os.localdomain>
2023-12-21 12:55:38 +0530

    blog site created

--- blog/info/views.py
+++ blog/info/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+# blog/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'studio/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'studio/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'studio/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'studio/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+
+    return render(request, 'studio/post_delete.html', {'post': post})
