1fb176801ae490f5d6daede56831eeb2c51d0aaf
xrig <xrig@pop-os.localdomain>
2023-12-21 12:52:43 +0530

    schema created/models added

--- blog/info/models.py
+++ blog/info/models.py
@@ -1,3 +1,11 @@
+# blog/models.py
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # post_id = models.AutoField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
